f3fc5265be0f39188fef9f350e3e1835d87788bc
Jennifer Haddock <jentx123@hotmail.com>
2017-10-07 03:13:33 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
 
-  resources :preferences
   resources :artists do
     resources :songs, only: [:index, :show]
   end
   resources :songs
 
+  namespace :admin do
+    resources :preference, only: [:index]
+  end
 end
