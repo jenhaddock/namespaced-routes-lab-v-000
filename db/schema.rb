f52bb5db957a8b77a8b5ff5235fd171105f0b291
Jennifer Haddock <jentx123@hotmail.com>
2017-10-07 03:10:12 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160126235547) do
+ActiveRecord::Schema.define(version: 20171007030902) do
 
   create_table "artists", force: :cascade do |t|
     t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "preferences", force: :cascade do |t|
+    t.string   "sort_order"
+    t.string   "string"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
