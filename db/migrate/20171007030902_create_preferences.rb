d902f6812a3d17071ff82d2b0ff082e7c53db654
Jennifer Haddock <jentx123@hotmail.com>
2017-10-07 03:32:45 +0000

    automatically backed up by learn

--- db/migrate/20171007030902_create_preferences.rb
+++ db/migrate/20171007030902_create_preferences.rb
@@ -2,7 +2,6 @@ class CreatePreferences < ActiveRecord::Migration
   def change
     create_table :preferences do |t|
       t.string :sort_order
-      t.string :string
 
       t.timestamps null: false
     end
